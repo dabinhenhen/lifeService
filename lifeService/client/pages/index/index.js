0833643f9772fb585872debf011ffe99db3fdb24
dabinhenhen <578670822@qq.com>
2018-04-01 08:10:18 +0800

    功能按钮追加
    功能按钮布局追加

--- lifeService/client/pages/index/index.js
+++ lifeService/client/pages/index/index.js
@@ -13,6 +13,32 @@ Page({
     indicatorDots: true,
     autoplay: true,
     interval: 4000,
-    duration: 1000
+    duration: 500,
+    //[ 当前显示的Imageurl ]
+    currentImageUrl: 0,
+
+    //[ 功能按钮 ]
+    funcIcons: [
+      { image: '../../images/infor.png', url: './index', text: '美容' },
+      { image: '../../images/infor_selected.png', url: './index', text: '玩乐' },
+      { image: '../../images/infor.png', url: './index', text: '美食' },
+      { image: '../../images/infor.png', url: './index', text: '政策' },
+      { image: '../../images/infor.png', url: './index', text: '母婴' },
+      { image: '../../images/infor.png', url: './index', text: '健康' },
+      { image: '../../images/chat.png', url: '../chat/chat', text: '聊天' },
+    ]
   },
+
+  //[ Swipe改变事件 ]
+  bindchange: function (e) {
+    this.setData({
+      currentImageUrl: e.detail.current
+    })
+  },
+
+  //[ Image点击事件 ]
+  onSwipClick: function (e) {
+    //console.log(this.data.imgUrls[this.data.currentImageUrl])
+    util.showSuccess('tap image ' + (this.data.currentImageUrl + 1));
+  }
 })
